7255b4891825fb1137a202343ca3e79d7bf39468
BryanMBORijnland <48950699+BryanMBORijnland@users.noreply.github.com>
2019-09-12 14:24:19 +0200

    Create Form.php

---
+++ Form.php
@@ -0,0 +1,28 @@
+<html>
+<link rel="stylesheet" href="Stylesheet.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
+<title></title>
+<head>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col order-last">
+            First, but last
+        </div>
+        <div class="col">
+            Second, but unordered
+        </div>
+        <div class="col order-first">
+            Third, but first
+        </div>
+    </div>
+</div>
+
+</body>
+<?php
+?>
+</html>
