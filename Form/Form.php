a52b1981b9dd59dfd8f4a822e22a2aee8a46cbf5
BryanMBORijnland <48950699+BryanMBORijnland@users.noreply.github.com>
2019-09-20 11:38:41 +0200

    Resturcture of directories

---
+++ Form/Form.php
@@ -0,0 +1,41 @@
+<html>
+<link rel="stylesheet" href="../CSS/Stylesheet.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
+<title></title>
+<head>
+</head>
+<body>
+
+<div class="container outer-frame-pos">
+    <div class="row justify-content-center">
+        <div class="col"></div>
+        <div class="col outer-frame rounded" align="center">
+            <form action="/">
+                First name:<br>
+                <input type="text" name="firstname">
+                <br><br>
+                Last name:<br>
+                <input type="text" name="lastname">
+                <br><br>
+                Adres:<br>
+                <input type="text" name="Adres">
+                <br><br>
+                Residence:<br>
+                <input type="text" name="Residence">
+                <br><br>
+                Date of birth:<br>
+                <input type="number" name="DOB">
+                <br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
+
+</body>
+<?php
+?>
+</html>
